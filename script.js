ffb08bde1510e152ee3180368a992e5c7000a1e0
TraBee <tra8brown@gmail.com>
2021-05-13 11:07:39 -0600

    updated with a few codes

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 // Assignment code here
+var numberCharacters = window.confirm("how many characters would you like your password to contain?");
+console.log()
+if (numberCharacters === true) {
+    window.alert();
+} else {
+    window.alert();
+}
 
+var specialCharacters = window.confirm("click OK to confirm including special characters.");
+console.log()
+if (numberCharacters === true) {
+    window.alert();
+} else {
+    window.alert();
+}
+
+var numericCharacters = window.confirm("click OK to confirm including numeric characters.");
+console.log()
+if (numberCharacters === true) {
+    window.alert();
+} else {
+    window.alert();
+}
+
+var lowerCase = window.confirm("click OK to confirm including lower case characters.");
+console.log()
+if (numberCharacters === true) {
+    window.alert();
+} else {
+    window.alert();
+}
+
+var upperCase = window.confirm("click OK to confirm including upper case characters.");
+console.log()
+if (numberCharacters === true) {
+    window.alert();
+} else {
+    window.alert();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+    var password = "password";
+    return password;
+}
 
 // Write password to the #password input
 function writePassword() {
